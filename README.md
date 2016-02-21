945d678e60eee968d77809087191ca0e8e41b063
Rense VanderHoek <rense@me.com>
2016-02-21 18:26:01 +0100

    Added minimal python/bottle hangman

--- README.md
+++ README.md
@@ -1,1 +1,59 @@
-# hangman
+# Hangman Assignment
+
+### Description
+
+Implement a minimal version of a hangman application using this API spec:
+
+| Method | URL | Description |
+| - | - | - |
+| POST  | /games  | Start a new game |
+| GET  | /games  | Overview of all games |
+| GET  | /games/{id}  | JSON response that should at least include:<br />__word__: representation of the word that is being guessed. Should contain dots for letters that have not been guessed yet (e.g. "aw..so..")<br />__tries_left__: the number of tries left to guess the word (starts at 11)<br />__status__: current status of the game (busy\|fail\|success) |
+| POST  | /games/{id}  | Guessing a letter, <br />POST body: ```char=a``` |
+
+Guessing a correct letter doesn’t decrement the amount of tries left.<br />
+Only valid characters are a-z (lowercase).
+
+### Notes
+* URLs have no ending slashes;
+* Unclear about JSON responses. For example, /games/ POST should return at least a new game-id;
+* Edge case handling unclear. For example, repeating the same character in /game/{id} POST;
+* Returning the 'already used characters' in a game JSON response would be very convenient.
+
+---
+# Submissions
+
+The /data/ directory contains an english wordlist-file and can/will be used to store sqlite3-database files.
+
+### Hangman Minimal (Python/Bottle)
+
+#### Description
+A minimal version using only Python 2.7.10 stdlib and Bottle (http://bottlepy.org/). It uses sqlite3 (no ORM) for saving game-state.
+
+
+Run it with:
+
+```bash
+$ cd minimal; virtualenv minimal; source minimal/bin/activate
+$ easy_install bottle
+$ python main.py
+```
+Then take your favourite HTTP-client and go to:
+
+| Method | URL | Description |
+| - | - | - |
+| GET | http://127.0.0.1:8080/games | Returns JSON with a list of games. |
+| POST | http://127.0.0.1:8080/games | Creates a new game (returns game-data JSON). |
+| GET |  http://127.0.0.1:8080/games/{id} | Returns game-data JSON. |
+| POST |  http://127.0.0.1:8080/games/{id} | Play the game by guessing a character.<br />Expects POST body with ```char=x``` where x is a single alphabetical character. Returns game-data JSON. |
+
+Response game-data example JSON:
+```json
+{
+    "id": "55b6519e1f0f465fb63e36d55abea5a7",
+    "status": "busy", 
+    "tries_left": 9, 
+    "word": ".a....."
+}
+```
+
---
+++ minimal/main.py
@@ -0,0 +1,159 @@
+#!/usr/bin/env python
+import os
+import json
+import uuid
+import random
+import sqlite3
+
+from httplib import CREATED, NOT_FOUND, BAD_REQUEST, FORBIDDEN
+from bottle import get, post, request, response, run
+
+STATUS_BUSY = 0
+STATUS_FAIL = 1
+STATUS_SUCCESS = 2
+
+base_dir = os.path.dirname(os.path.abspath(__file__))
+WORD_LIST = list(open(os.path.join(base_dir, '..', 'data', 'wordlist.txt')))
+DB_FILE = os.path.join(base_dir, '..', 'data', 'minimal.sqlite3')
+
+
+@get('/games')
+def game_list():
+    """ Return list of games """
+    result = _db_query(
+        "SELECT key, word_to_guess, used_characters, tries_left, "
+        "status FROM games"
+    )
+    games = [_output(game) for game in result]
+    return json.dumps(games)
+
+
+@get('/games/<key:re:[a-z0-9]+>')
+def game_detail(key):
+    """ Return game-detail """
+    game = _db_get_game(key)
+    if not game:
+        response.status = NOT_FOUND
+        return
+    return json.dumps(_output(game))
+
+
+@post('/games')
+def game_start():
+    """ Start/create a new game """
+    key = uuid.uuid4().hex
+    _db_query(
+        "INSERT INTO games (key, word_to_guess, used_characters, "
+        "tries_left, status) VALUES (?, ?, '', 11, 0)",
+        key, random.choice(WORD_LIST).strip()
+    )
+    game = _db_get_game(key)
+    response.status = CREATED
+    return json.dumps(_output(game))
+
+
+@post('/games/<key:re:[a-z0-9]+>')
+def game_play(key):
+    """ Play game """
+    if not request.params.get('char'):
+        response.status = BAD_REQUEST
+        return
+    character = request.params.get('char').lower()
+    if len(character) > 1 or not character.isalpha():
+        response.status = BAD_REQUEST
+        return
+
+    game = _db_get_game(key)
+    if not game:
+        response.status = NOT_FOUND
+        return
+
+    status = game['status']
+    if status != STATUS_BUSY:
+        response.status = FORBIDDEN
+        return
+
+    if character in game['used_characters']:
+        # Requirements unclear here, do nothing...
+        print 'koekoek'
+        return json.dumps(_output(_db_get_game(key)))
+    used_characters = game['used_characters'] + character
+
+    tries_left = game['tries_left']
+    if character not in game['word_to_guess']:
+        # Decrement tries_left
+        tries_left -= 1
+
+    word = _dottify(game['word_to_guess'], used_characters)
+    if word == game['word_to_guess']:
+        # Player guessed the word!
+        status = STATUS_SUCCESS
+    elif tries_left <= 0:
+        # Player failed to guess the word
+        status = STATUS_FAIL
+
+    # Update game-state
+    _db_query(
+        "UPDATE games SET used_characters=?, tries_left=?, status=? "
+        "WHERE key=?", used_characters, tries_left, status, key
+    )
+    return json.dumps(_output(_db_get_game(key)))
+
+
+def _dottify(word_to_guess, used_characters):
+    """ Replace not yet guessed characters with dots """
+    word = word_to_guess
+    for character in word_to_guess:
+        if character not in used_characters:
+            word = word.replace(character, '.')
+    return word
+
+
+def _output(data):
+    """ Format game-data for client-response """
+    game = {
+        'id': data['key'],
+        'tries_left': data['tries_left'],
+        'status': ['busy', 'fail', 'succes'][data['status']],
+    }
+    if data['status'] != STATUS_BUSY:
+        # This game has ended, show the word
+        game['word'] = data['word_to_guess']
+    else:
+        print data
+        game['word'] = _dottify(data['word_to_guess'], data['used_characters'])
+    return game
+
+
+def _db_query(query, *args):
+    """ Handle database-connection, do queries, return results
+    """
+    if not os.path.exists(DB_FILE):
+        # Create our database/table, because it's missing
+        conn = sqlite3.connect(DB_FILE)
+        conn.execute("""
+            CREATE TABLE games(
+            id INTEGER PRIMARY KEY, key char(40) NOT NULL DEFAULT '',
+            word_to_guess char(22) NOT NULL DEFAULT '',
+            used_characters char(26) DEFAULT NULL,
+            tries_left int(2) DEFAULT 11, status int(1) DEFAULT 0)
+        """)
+    conn = sqlite3.connect(DB_FILE)
+    conn.row_factory = sqlite3.Row
+    _cursor = conn.cursor()
+    _cursor.execute(query, args)
+    conn.commit()
+    result = _cursor.fetchall()
+    _cursor.close()
+    return result
+
+
+def _db_get_game(key):
+    """ Convenience function for getting a single game by key """
+    return _db_query(
+        "SELECT key, word_to_guess, used_characters, tries_left, status "
+        "FROM games WHERE key = ?", key
+    )[0]
+
+
+run(reloader=True)
